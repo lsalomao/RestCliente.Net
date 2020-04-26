1b8216866a6996360cc0f1799a1375aa5c148229
Christian Findlay <a@a.com>
2020-04-27 07:07:41 +1000

    Fix more compilation issues

--- RestClient.Net/Client - .NET.cs
+++ RestClient.Net/Client - .NET.cs
@@ -1,22 +1,16 @@
-﻿#if NETCOREAPP3_0 || NETSTANDARD2_0
+﻿#if NET4_5
 
-using Microsoft.Extensions.Logging;
-using RestClient.Net.Abstractions;
-using RestClient.Net.Abstractions.Extensions;
 using System;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace RestClient.Net
 {
     public sealed partial class Client
     {
-
         /// <summary>
         /// Construct a client.
         /// </summary>
         /// <param name="serializationAdapter">The serialization adapter for serializing/deserializing http content bodies. Defaults to JSON and adds the default Content-Type header for JSON</param>
+        /// <param name="serializationAdapter">The serialization adapter for serializing/deserializing http content bodies. 
         /// <param name="name">The of the client instance. This is also passed to the HttpClient factory to get or create HttpClient instances</param>
         /// <param name="baseUri">The base Url for the client. Specify this if the client will be used for one Url only</param>
         /// <param name="defaultRequestHeaders">Default headers to be sent with http requests</param>
@@ -25,9 +19,7 @@ namespace RestClient.Net
         /// <param name="sendHttpRequestFunc">The Func responsible for performing the SendAsync method on HttpClient. This can replaced in the constructor in order to implement retries and so on.</param>
         /// <param name="requestConverter">IRequestConverter instance responsible for converting rest requests to http requests</param>
         public Client(
-
-           ISerializationAdapter serializationAdapter = null,
-
+            ISerializationAdapter serializationAdapter,
             string name = null,
             Uri baseUri = null,
             IHeadersCollection defaultRequestHeaders = null,
@@ -37,36 +29,13 @@ namespace RestClient.Net
             IRequestConverter requestConverter = null)
         {
             DefaultRequestHeaders = defaultRequestHeaders ?? new RequestHeadersCollection();
-
-            if (serializationAdapter == null)
-            {
-                SerializationAdapter = new JsonSerializationAdapter();
-                this.SetJsonContentTypeHeader();
-            }
-            else
-            {
-                SerializationAdapter = serializationAdapter;
-            }
-
+            SerializationAdapter = serializationAdapter ?? throw new ArgumentNullException(nameof(serializationAdapter));
             Logger = logger;
             BaseUri = baseUri;
             Name = name ?? Guid.NewGuid().ToString();
             RequestConverter = requestConverter ?? new DefaultRequestConverter(Logger);
             HttpClientFactory = httpClientFactory ?? new DefaultHttpClientFactory();
             _sendHttpRequestFunc = sendHttpRequestFunc ?? DefaultSendHttpRequestMessageFunc;
-        }
-
-        /// <summary>
-        /// Construct a client
-        /// </summary>
-        /// <param name="baseUri">The base Url for the client. Specify this if the client will be used for one Url only</param>
-        public Client(
-            Uri baseUri)
-        : this(
-            null,
-            null,
-            baseUri)
-        {
         }
     }
 }
--- RestClient.Net/Client.cs
+++ RestClient.Net/Client.cs
@@ -5,10 +5,6 @@ using RestClient.Net.Abstractions.Logging;
 using Microsoft.Extensions.Logging;
 #endif
 
-#if NETCOREAPP3_0 || NETSTANDARD2_0
-using RestClient.Net.Abstractions.Extensions;
-#endif
-
 using RestClient.Net.Abstractions;
 using System;
 using System.Linq;
@@ -111,9 +107,6 @@ namespace RestClient.Net
         #endregion
 
         #region Constructors
-
-
-
         /// <summary>
         /// Construct a client
         /// </summary>
@@ -159,58 +152,6 @@ namespace RestClient.Net
             httpClientFactory: httpClientFactory)
         {
         }
-        /// <summary>
-        /// Construct a client.
-        /// </summary>
-        /// <param name="serializationAdapter">The serialization adapter for serializing/deserializing http content bodies. Defaults to JSON and adds the default Content-Type header for JSON</param>
-        /// <param name="serializationAdapter">The serialization adapter for serializing/deserializing http content bodies. 
-#if NETCOREAPP3_0
-        /// Defaults to JSON and adds the default Content-Type header for JSON</param>
-#endif
-        /// <param name="name">The of the client instance. This is also passed to the HttpClient factory to get or create HttpClient instances</param>
-        /// <param name="baseUri">The base Url for the client. Specify this if the client will be used for one Url only</param>
-        /// <param name="defaultRequestHeaders">Default headers to be sent with http requests</param>
-        /// <param name="logger">Logging abstraction that will trace request/response data and log events</param>
-        /// <param name="httpClientFactory">The IHttpClientFactory instance that is used for getting or creating HttpClient instances when the SendAsync call is made</param>
-        /// <param name="sendHttpRequestFunc">The Func responsible for performing the SendAsync method on HttpClient. This can replaced in the constructor in order to implement retries and so on.</param>
-        /// <param name="requestConverter">IRequestConverter instance responsible for converting rest requests to http requests</param>
-        public Client(
-#if !NETCOREAPP3_0 && !NETSTANDARD2_0
-           ISerializationAdapter serializationAdapter,
-#else
-           ISerializationAdapter serializationAdapter = null,
-#endif
-            string name = null,
-            Uri baseUri = null,
-            IHeadersCollection defaultRequestHeaders = null,
-            ILogger logger = null,
-            IHttpClientFactory httpClientFactory = null,
-            Func<HttpClient, Func<HttpRequestMessage>, ILogger, CancellationToken, Task<HttpResponseMessage>> sendHttpRequestFunc = null,
-            IRequestConverter requestConverter = null)
-        {
-            DefaultRequestHeaders = defaultRequestHeaders ?? new RequestHeadersCollection();
-
-#if !NETCOREAPP3_0 && !NETSTANDARD2_0
-            SerializationAdapter = serializationAdapter ?? throw new ArgumentNullException(nameof(serializationAdapter));
-#else
-            if (serializationAdapter == null)
-            {
-                SerializationAdapter = new JsonSerializationAdapter();
-                this.SetJsonContentTypeHeader();
-            }
-            else
-            {
-                SerializationAdapter = serializationAdapter;
-            }
-#endif
-            Logger = logger;
-            BaseUri = baseUri;
-            Name = name ?? Guid.NewGuid().ToString();
-            RequestConverter = requestConverter ?? new DefaultRequestConverter(Logger);
-            HttpClientFactory = httpClientFactory ?? new DefaultHttpClientFactory();
-            _sendHttpRequestFunc = sendHttpRequestFunc ?? DefaultSendHttpRequestMessageFunc;
-        }
-
         #endregion
 
         #region Implementation
@@ -367,7 +308,6 @@ namespace RestClient.Net
         }
         #endregion
     }
-
 }
 
 #pragma warning restore CA2000
